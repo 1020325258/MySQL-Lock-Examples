160a147333fc660bd529a26532ff4fabf0561381
11来了 <zhai_q_y@163.com>
2025-08-11 23:16:58 +0800

    refactor:提取公共代码

--- src/main/java/lock/lock1.java
+++ src/main/java/lock/lock1.java
@@ -20,7 +20,9 @@ public class lock1 extends BaseClass{
          * 因此，在 c 索引树上添加的锁为：(0, 5]、(5, 10)。
          * select ... for update 会认为后续想要更新对应的数据，因此除了在 c 索引树上加锁之外，还会在主键索引上加锁。
          * 同样，先在 (0, 5] 添加 next-key lock，由于主键索引也是唯一索引，next-key lock 会退化为行锁，因此主键索引只针对了 id = 5 添加了行锁。
-         * 总结：select id from t where c=5 for update 在 c 索引上添加了 (0, 5] 和 (5, 10) 的锁，在住建索引上添加了 id = 5 的行锁。
+         * 总结：
+         * 1、c 索引： (0, 5] 和 (5, 10) 的锁。
+         * 2、主键索引：id = 5 的行锁。
          */
         String lockSQL = "select id from t where c=5 for update";
 
